65c0fdcb000902d364c1e5278cfe3deb6a44c94c
Nenavath Naresh <nenavathnaresh460@gmail.com>
2024-11-06 00:40:01 +0530

    5-11-24

---
+++ Matrix/Medium/rotate-by-90-degree.py
@@ -0,0 +1,13 @@
+def rotate90Clockwise(matrix):
+    n = len(matrix)
+    
+    # Step 1: Transpose the matrix
+    for i in range(n):
+        for j in range(i + 1, n):
+            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+    
+    # Step 2: Reverse each row
+    for i in range(n):
+        matrix[i].reverse()
+
+    return matrix
